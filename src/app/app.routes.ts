e660205619a9950bdd3e75c1a107f668cb358ade
Hunjoon Rhee <hunjoonrhee90@gmail.com>
2026-05-07 19:02:23 +0200

    fix route bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { TripsOverview } from './components/trips-overview/trips-overview';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'overview' },
   {
-    path: '/overview',
+    path: 'overview',
     component: TripsOverview,
   },
   {
     path: '**',
-    redirectTo: '/overview',
+    redirectTo: 'overview',
   },
 ];
